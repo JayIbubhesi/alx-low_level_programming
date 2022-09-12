77a1f0d46df37e1d926a105608e0bb4f40ce973a
Jabulani <tjabulani8@gmail.com>
2022-09-12 14:20:31 -0700

    test7

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit of %i is %i and is less then 6 and not 0\n", n, l);
+	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
